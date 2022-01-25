8e948f8b5922e9902e3c527d935857cbc569eec8
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-01-25 13:20:27 -0300

    Remove commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,6 @@ class ApplicationController < ActionController::Base
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
-  # def after_sign_in_path_for(resource)
-  #    redirect_to root_path
-  # end
   def after_inactive_sign_up_path_for
     redirect_to root_path
   end
